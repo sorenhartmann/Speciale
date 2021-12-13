783838b521ae700b69770b6f1a81d55eb088675c
Søren Hartmann <soren.hartmann@gmail.com>
2021-12-13 17:32:06 +0100

    train mcmc

--- batch_scripts/mnist/train_mcmc_models.sh
+++ batch_scripts/mnist/train_mcmc_models.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
-#BSUB -q gpuv100
-#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -q hpc
 #BSUB -J train_mnist
-#BSUB -n 4
-#BSUB -W 5:00
+#BSUB -n 8
+#BSUB -W 10:00
 #BSUB -B
 #BSUB -N
 #BSUB -R span[hosts=1]
-#BSUB -R "rusage[mem=8GB]"
+#BSUB -R "rusage[mem=16GB]"
 #BSUB -o logs/%J.out
 #BSUB -e logs/%J.err
 
 module load python3/3.9.6 
-module load cuda/8.0
-module load cudnn/v7.0-prod-cuda8
 
 cd ~/Documents/Speciale
 source .venv/bin/activate
 
-python scripts/inference.py -m \
+python scripts/inference.py -m hydra/launcher=joblib\
     +experiment=mnist \
     experiment/mnist=glob(sghmc*) \
+    +extra_callbacks=save_samples_and_log_temp_all \
     ++trainer.max_epochs=1000 \
-    ++data.num_workers=3 \
-    ++trainer.gpus=1
+    ++data.num_workers=3
