19f63d124af14c2f48a3100281b20d4c860b12fd
s164182 <s164182@student.dtu.dk>
2021-12-14 18:35:55 +0100

    fixed time limit in script

--- batch_scripts/mnist/train_mcmc_models.sh
+++ batch_scripts/mnist/train_mcmc_models.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #BSUB -q hpc
 #BSUB -J train_mnist
-#BSUB -n 8
+#BSUB -n 2
 #BSUB -W 10:00
 #BSUB -B
 #BSUB -N
 #BSUB -R span[hosts=1]
-#BSUB -R "rusage[mem=16GB]"
+#BSUB -R "rusage[mem=8GB]"
 #BSUB -o logs/%J.out
 #BSUB -e logs/%J.err
 
@@ -17,7 +17,9 @@ source .venv/bin/activate
 
 python scripts/inference.py -m hydra/launcher=joblib\
     +experiment=mnist \
-    experiment/mnist=glob(sghmc*) \
+    experiment/mnist="glob(sghmc*)" \
     +extra_callbacks=save_samples_and_log_temp_all \
     ++trainer.max_epochs=1000 \
-    ++data.num_workers=3
+    ++data.num_workers=0 \
+    ++trainer.progress_bar_refresh_rate=0
+    trainer.max_time="00:09:00:00"
